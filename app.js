fe9192d9fb5ae69b443fbbf4ab47a78d71a0c731
Blad16 <abrahambladimir400@gmail.com>
2022-10-28 10:40:50 -0600

    lab numoer 1

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotnev').config()
+const express = require ('express')
+const app = express()
+app.get('/', function (rq,res){
+res.send('hello world ') 
+
+})
+app.listen(process.env.PORT, function(){
+
+    console.log("el puerto esta corriendo el puerto  TCP" + process.env.PORT)
+})
+
+
+
+
+
+
+// error require stack module not found//
+// error por no tener instalados los modulos de node js //
